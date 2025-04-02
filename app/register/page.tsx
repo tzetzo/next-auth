8c24d834f1541cbef80aa3e675e868a8070a8afd
Tzvetan Marinov <tmarinov@stamus-networks.com>
2025-04-02 17:02:31 +0200

    register user page and server action boilerplate

---
+++ app/register/page.tsx
@@ -0,0 +1,114 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { registerSchema, RegisterSchema } from "@/validation/userSchemas";
+import { registerUser } from "../actions";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+export default function Register() {
+  const form = useForm<RegisterSchema>({
+    resolver: zodResolver(registerSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+      passwordConfirm: "",
+    },
+  });
+
+  async function onSubmit(values: RegisterSchema) {
+    try {
+      const user = await registerUser(values);
+      console.log("User registered successfully:", user);
+    } catch (error: any) {
+      console.error("Registration failed:", error);
+    }
+  }
+
+  return (
+    <main className="flex h-screen w-full items-center justify-center">
+      <Card className="w-[350px] space-y-4 p-6 shadow-md">
+        <CardHeader>
+          <CardTitle>Register</CardTitle>
+          <CardDescription>Register for a new account</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Email</FormLabel>
+                    <FormControl>
+                      <Input placeholder="email@example.com" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Password</FormLabel>
+                    <FormControl>
+                      <Input
+                        type="password"
+                        placeholder="********"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="passwordConfirm"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Confirm Password</FormLabel>
+                    <FormControl>
+                      <Input
+                        type="password"
+                        placeholder="********"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <Button type="submit" className="w-full">
+                Register
+              </Button>
+            </form>
+          </Form>
+        </CardContent>
+        <CardFooter>
+          <p>Card Footer</p>
+        </CardFooter>
+      </Card>
+    </main>
+  );
+}
---
+++ validation/userSchemas.ts
@@ -0,0 +1,24 @@
+import { z } from "zod";
+
+// Email validation schema
+export const emailSchema = z.string().email("Invalid email address");
+
+// Password validation schema
+export const passwordSchema = z
+  .string()
+  .min(5, { message: "Password must be at least 5 characters." })
+  .max(50, { message: "Password must not exceed 50 characters." });
+
+// Full user registration schema
+export const registerSchema = z
+  .object({
+    email: emailSchema,
+    password: passwordSchema,
+    passwordConfirm: z.string(),
+  })
+  .refine((data) => data.password === data.passwordConfirm, {
+    message: "Passwords must match",
+    path: ["passwordConfirm"], // This specifies where the error will appear
+  });
+
+export type RegisterSchema = z.infer<typeof registerSchema>;
