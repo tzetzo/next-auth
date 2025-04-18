5fbd7d4f3884e7feebc2fe838c441571288fd153
Tzvetan Marinov <tmarinov@stamus-networks.com>
2025-04-18 14:07:59 +0300

    showing user email in /my-account page

--- app/(logged-in)/layout.tsx
+++ app/(logged-in)/layout.tsx
@@ -34,11 +34,7 @@ export default async function LoggedInLayout({
               Change Password
             </Link>
           </div>
-          {isLoggedIn && (
-            <span>
-              Logged in as {user?.email} <LogoutButton />
-            </span>
-          )}
+          {isLoggedIn && <LogoutButton />}
         </div>
       </nav>
       {children}
--- app/(logged-in)/my-account/page.tsx
+++ app/(logged-in)/my-account/page.tsx
@@ -1,7 +1,22 @@
-export default function MyAccount() {
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+
+import { auth } from "@/auth";
+import { Label } from "@/components/ui/label";
+
+export default async function MyAccount() {
+  const session = await auth();
+
   return (
-    <main className="flex h-screen w-full items-center justify-center">
-      my-account
-    </main>
+    <div className="flex h-screen w-full items-center justify-center">
+      <Card className="w-[350px] space-y-4 p-6 shadow-md ">
+        <CardHeader>
+          <CardTitle>My account</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <Label>Email address</Label>
+          <div className="text-muted-foreground">{session?.user?.email}</div>
+        </CardContent>
+      </Card>
+    </div>
   );
 }
